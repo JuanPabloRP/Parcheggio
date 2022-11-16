f4479fd74440921a65e2eaad79a071cf1091b45b
AlvaroG <ajesusgomez360@gmail.com>
2022-11-16 18:42:22 -0500

    Eliminar y Lugares disponibles melos

--- Parcheggio/EliminarPuesto.Designer.cs
+++ Parcheggio/EliminarPuesto.Designer.cs
@@ -29,52 +29,49 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EliminarPuesto));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btnCrearPuesto = new System.Windows.Forms.Button();
             this.txtCrearPuesto = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Parcheggio.Properties.Resources.eliminar;
+            this.pictureBox1.Location = new System.Drawing.Point(286, 103);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(229, 44);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 32;
+            this.pictureBox1.TabStop = false;
+            // 
             // btnCrearPuesto
             // 
-            this.btnCrearPuesto.Location = new System.Drawing.Point(365, 316);
+            this.btnCrearPuesto.Location = new System.Drawing.Point(360, 289);
             this.btnCrearPuesto.Name = "btnCrearPuesto";
             this.btnCrearPuesto.Size = new System.Drawing.Size(75, 23);
-            this.btnCrearPuesto.TabIndex = 26;
-            this.btnCrearPuesto.Text = "Crear";
+            this.btnCrearPuesto.TabIndex = 31;
+            this.btnCrearPuesto.Text = "Eliminar";
             this.btnCrearPuesto.UseVisualStyleBackColor = true;
-            this.btnCrearPuesto.Click += new System.EventHandler(this.btnCrearPuesto_Click);
             // 
             // txtCrearPuesto
             // 
-            this.txtCrearPuesto.Location = new System.Drawing.Point(284, 175);
+            this.txtCrearPuesto.Location = new System.Drawing.Point(277, 229);
             this.txtCrearPuesto.Name = "txtCrearPuesto";
-            this.txtCrearPuesto.Size = new System.Drawing.Size(144, 20);
-            this.txtCrearPuesto.TabIndex = 25;
-            this.txtCrearPuesto.TextChanged += new System.EventHandler(this.txtCrearPuesto_TextChanged);
+            this.txtCrearPuesto.Size = new System.Drawing.Size(244, 20);
+            this.txtCrearPuesto.TabIndex = 30;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(281, 159);
+            this.label2.Location = new System.Drawing.Point(274, 203);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(88, 13);
-            this.label2.TabIndex = 24;
-            this.label2.Text = "Ingrese el puesto";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(295, 77);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(183, 31);
-            this.label1.TabIndex = 22;
-            this.label1.Text = "Crear puesto";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label2.Size = new System.Drawing.Size(135, 13);
+            this.label2.TabIndex = 29;
+            this.label2.Text = "Ingrese el puesto a eliminar";
             // 
             // pictureBox2
             // 
@@ -82,26 +79,26 @@
             this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.pictureBox2.ErrorImage = null;
             this.pictureBox2.InitialImage = null;
-            this.pictureBox2.Location = new System.Drawing.Point(232, 130);
+            this.pictureBox2.Location = new System.Drawing.Point(232, 167);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(337, 243);
+            this.pictureBox2.Size = new System.Drawing.Size(337, 180);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 23;
+            this.pictureBox2.TabIndex = 28;
             this.pictureBox2.TabStop = false;
-            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
             // EliminarPuesto
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnCrearPuesto);
             this.Controls.Add(this.txtCrearPuesto);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.label1);
             this.Name = "EliminarPuesto";
             this.Text = "EliminarPuesto";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -110,10 +107,10 @@
 
         #endregion
 
+        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button btnCrearPuesto;
         private System.Windows.Forms.TextBox txtCrearPuesto;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.Label label1;
     }
 }
