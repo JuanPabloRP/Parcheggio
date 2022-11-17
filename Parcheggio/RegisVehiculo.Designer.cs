7fb6bb653907ba50fbf2361701c41b25e383d371
Sakuta <travispastrana075@gmail.com>
2022-11-16 19:53:01 -0500

    asas

--- Parcheggio/RegisVehiculo.Designer.cs
+++ Parcheggio/RegisVehiculo.Designer.cs
@@ -37,7 +37,7 @@
             this.txtcolor = new System.Windows.Forms.TextBox();
             this.txtplaca = new System.Windows.Forms.TextBox();
             this.cbPuestoVehi = new System.Windows.Forms.ComboBox();
-            this.cbTipoVehi = new System.Windows.Forms.ComboBox();
+            this.Cbtipo = new System.Windows.Forms.ComboBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.btnRogVeih = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
@@ -50,7 +50,7 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.White;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(205, 123);
+            this.label1.Location = new System.Drawing.Point(316, 160);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(47, 16);
             this.label1.TabIndex = 0;
@@ -61,7 +61,7 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.White;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(538, 123);
+            this.label2.Location = new System.Drawing.Point(649, 160);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(44, 16);
             this.label2.TabIndex = 1;
@@ -72,7 +72,7 @@
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.White;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(205, 257);
+            this.label3.Location = new System.Drawing.Point(316, 294);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(39, 16);
             this.label3.TabIndex = 2;
@@ -84,7 +84,7 @@
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.White;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(538, 257);
+            this.label4.Location = new System.Drawing.Point(649, 294);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(111, 16);
             this.label4.TabIndex = 3;
@@ -94,7 +94,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(225, 15);
+            this.label5.Location = new System.Drawing.Point(336, 52);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(357, 46);
             this.label5.TabIndex = 4;
@@ -102,47 +102,49 @@
             // 
             // txtcolor
             // 
-            this.txtcolor.Location = new System.Drawing.Point(541, 151);
+            this.txtcolor.Location = new System.Drawing.Point(652, 188);
             this.txtcolor.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtcolor.Name = "txtcolor";
             this.txtcolor.Size = new System.Drawing.Size(108, 22);
             this.txtcolor.TabIndex = 6;
-            this.txtcolor.TextChanged += new System.EventHandler(this.txtcolor_TextChanged);
             // 
             // txtplaca
             // 
-            this.txtplaca.Location = new System.Drawing.Point(208, 151);
+            this.txtplaca.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtplaca.Location = new System.Drawing.Point(319, 188);
             this.txtplaca.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtplaca.MaxLength = 6;
             this.txtplaca.Name = "txtplaca";
             this.txtplaca.Size = new System.Drawing.Size(108, 22);
             this.txtplaca.TabIndex = 7;
-            this.txtplaca.TextChanged += new System.EventHandler(this.txtplaca_TextChanged);
             // 
             // cbPuestoVehi
             // 
             this.cbPuestoVehi.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.cbPuestoVehi.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbPuestoVehi.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.cbPuestoVehi.FormattingEnabled = true;
-            this.cbPuestoVehi.Location = new System.Drawing.Point(541, 288);
+            this.cbPuestoVehi.Location = new System.Drawing.Point(652, 325);
             this.cbPuestoVehi.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cbPuestoVehi.Name = "cbPuestoVehi";
             this.cbPuestoVehi.Size = new System.Drawing.Size(119, 24);
             this.cbPuestoVehi.TabIndex = 8;
             // 
-            // cbTipoVehi
+            // Cbtipo
             // 
-            this.cbTipoVehi.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.cbTipoVehi.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.cbTipoVehi.FormattingEnabled = true;
-            this.cbTipoVehi.Items.AddRange(new object[] {
-            "Automovil\t",
-            "Motocicleta"});
-            this.cbTipoVehi.Location = new System.Drawing.Point(208, 288);
-            this.cbTipoVehi.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.cbTipoVehi.Name = "cbTipoVehi";
-            this.cbTipoVehi.Size = new System.Drawing.Size(119, 24);
-            this.cbTipoVehi.TabIndex = 9;
-            this.cbTipoVehi.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            this.Cbtipo.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.Cbtipo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Cbtipo.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Cbtipo.FormattingEnabled = true;
+            this.Cbtipo.Items.AddRange(new object[] {
+            "Motocicleta",
+            "Automovil"});
+            this.Cbtipo.Location = new System.Drawing.Point(319, 325);
+            this.Cbtipo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Cbtipo.Name = "Cbtipo";
+            this.Cbtipo.Size = new System.Drawing.Size(119, 24);
+            this.Cbtipo.TabIndex = 9;
+            this.Cbtipo.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
             // pictureBox2
             // 
@@ -150,7 +152,7 @@
             this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.pictureBox2.ErrorImage = null;
             this.pictureBox2.InitialImage = null;
-            this.pictureBox2.Location = new System.Drawing.Point(110, 83);
+            this.pictureBox2.Location = new System.Drawing.Point(221, 120);
             this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(586, 366);
@@ -160,13 +162,14 @@
             // 
             // btnRogVeih
             // 
-            this.btnRogVeih.Location = new System.Drawing.Point(358, 367);
+            this.btnRogVeih.Location = new System.Drawing.Point(469, 404);
             this.btnRogVeih.Margin = new System.Windows.Forms.Padding(4);
             this.btnRogVeih.Name = "btnRogVeih";
             this.btnRogVeih.Size = new System.Drawing.Size(147, 34);
             this.btnRogVeih.TabIndex = 20;
             this.btnRogVeih.Text = "Registrar vehiculo";
             this.btnRogVeih.UseVisualStyleBackColor = true;
+            this.btnRogVeih.Click += new System.EventHandler(this.btnRogVeih_Click);
             // 
             // pictureBox1
             // 
@@ -186,10 +189,10 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(804, 483);
+            this.ClientSize = new System.Drawing.Size(1072, 594);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnRogVeih);
-            this.Controls.Add(this.cbTipoVehi);
+            this.Controls.Add(this.Cbtipo);
             this.Controls.Add(this.cbPuestoVehi);
             this.Controls.Add(this.txtplaca);
             this.Controls.Add(this.txtcolor);
@@ -200,8 +203,8 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox2);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.MaximumSize = new System.Drawing.Size(822, 530);
-            this.MinimumSize = new System.Drawing.Size(822, 530);
+            this.MaximumSize = new System.Drawing.Size(1090, 641);
+            this.MinimumSize = new System.Drawing.Size(1090, 641);
             this.Name = "RegisVehiculo";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Load += new System.EventHandler(this.RegisVehiculo_Load);
@@ -222,7 +225,7 @@
         private System.Windows.Forms.TextBox txtcolor;
         private System.Windows.Forms.TextBox txtplaca;
         private System.Windows.Forms.ComboBox cbPuestoVehi;
-        private System.Windows.Forms.ComboBox cbTipoVehi;
+        private System.Windows.Forms.ComboBox Cbtipo;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Button btnRogVeih;
         private System.Windows.Forms.PictureBox pictureBox1;
--- Parcheggio/clases/Cliente.cs
+++ Parcheggio/clases/Cliente.cs
@@ -8,22 +8,20 @@ namespace Parcheggio.clases
 {
     public class Cliente
     {
+
+        public static List<Cliente> cliente = new List<Cliente>();
+
         public int id { get; set; }
         public string tipo { get; set; }
         public string placa { get; set; }
         public string color { get; set; }
-        public double totalAPagar { get; set; }
 
-        public Cliente(int _id)
+        public Cliente(int _id, string _tipo, string _placa, string _color)
         {
             id = _id;
-            tipo = null;
-            placa = null;
-            color = null;
-            totalAPagar = 0;
+            tipo = _tipo;
+            placa = _placa;
+            color = _color;
         }
-
-
-
     }
 }
